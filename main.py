c6589bed1685e8788792a2dbf5faabd0d0a3759a
Lumixbose <bossbew001@gmail.com>
2024-02-14 20:33:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import streamlit as st
-st.header("Shettawut Thayaping")
-st.header("เสฏฐวุฒิ ถายะพิงค์")
-st.write("64545202050-8")
-st.write(":D")
+import pyttsx3
+
+def text_to_speech(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def main():
+    st.title("Text-to-Speech Audio Book")
+
+    # Input text area
+    text = st.text_area("Enter text")
+
+    # Button to convert text to speech
+    if st.button("Convert to Audio"):
+        if text:
+            st.audio(text_to_speech(text), format="audio/wav")
+        else:
+            st.warning("Please enter some text")
+
+if __name__ == "__main__":
+    main()
